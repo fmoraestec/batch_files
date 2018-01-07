0b7d387aa30496588a7185069f4db216a9283f99
fmoraestec <34696450+fmoraestec@users.noreply.github.com>
2018-01-07 16:33:20 -0200

    Update run_all.bat

--- run_all.bat
+++ run_all.bat
@@ -1,20 +1,20 @@
 @echo off
 if exist "C:\clnhk" (
-start /d "C:\IE+Chrome+Mozila.bat" IE+Chrome+Mozila.bat
+start /d "C:\clnhk\IE+Chrome+Mozila.bat" IE+Chrome+Mozila.bat
 )
 @echo off
 if exist "C:\clnhk" (
-start /d "C:\chrome_wipe.bat" chrome_wipe.bat
+start /d "C:\clnhk\chrome_wipe.bat" chrome_wipe.bat
 )
 @echo off
 if exist "C:\clnhk" (
-start /d "C:\mozila_wipe.bat" mozila_wipe.bat
+start /d "C:\clnhk\mozila_wipe.bat" mozila_wipe.bat
 )
 @echo off
 if exist "C:\clnhk" (
-start /d "C:\thumb_clean.bat" thumb_clean.bat
+start /d "C:\clnhk\thumb_clean.bat" thumb_clean.bat
 )
 @echo off
 if exist "C:\clnhk" (
-start /d "C:\shutdown+time.bat" shutdown+time.bat
+start /d "C:\clnhk\shutdown+time.bat" shutdown+time.bat
 )
