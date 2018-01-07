13788db92d6f6b3aa4e2a451197d8ac702e51334
fmoraestec <34696450+fmoraestec@users.noreply.github.com>
2018-01-07 12:04:54 -0200

    Update run_all.bat

--- run_all.bat
+++ run_all.bat
@@ -1,10 +1,44 @@
 @echo off
-if exist "C:\" (
-start /d "C:\" CCleaner.exe /AUTO
+if exist "C:\clnhk" (
+start /d "C:\Ccleaner_auto.bat" Ccleaner_auto.bat /AUTO
 )
-
-if exist "%programfiles%\CCleaner" (
-start /d "C:\Program Files\CCleaner" CCleaner.exe /AUTO
+@echo off
+if exist "C:\clnhk" (
+start /d "C:\Ccleaner_auto.bat" Ccleaner_auto.bat
 )
+@echo off
+if exist "C:\clnhk" (
+start /d "C:\ChangeMACAddressBatch.bat" ChangeMACAddressBatch.bat
+)
+@echo off
+if exist "C:\clnhk" (
+start /d "C:\IE+Chrome+Mozila.bat" IE+Chrome+Mozila.bat
+)
+@echo off
+if exist "C:\clnhk" (
+start /d "C:\chrome_wipe.bat" chrome_wipe.bat
+)
+@echo off
+if exist "C:\clnhk" (
+start /d "C:\event_clear_win.bat" event_clear_win.bat
+)
+@echo off
+if exist "C:\clnhk" (
+start /d "C:\mozila_wipe.bat" mozila_wipe.bat
+)
+@echo off
+if exist "C:\clnhk" (
+start /d "C:\shutdown+time.bat" shutdown+time.bat
+)
+@echo off
+if exist "C:\clnhk" (
+start /d "C:\thumb_clean.bat" thumb_clean.bat
+)
+@echo off
+if exist "C:\clnhk" (
+start /d "C:\mozila_wipe.bat" mozila_wipe.bat /AUTO
+)
+@echo off
+ - Shutdown your pc within X minutes.
 
-time /T >> %appdata%\CCleaner_history.txt
+ - Wipe Thumb.db Logs from all drives of system. It is very usefull cause thumb cache contains huge information
